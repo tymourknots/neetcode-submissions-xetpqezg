1fd66c2ecae4cf9d95e0dadfcf0ba8cd587fddaa
Tymour Aidabole <146785683+tymourknots@users.noreply.github.com>
2026-04-14 20:24:12 -0400

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.py
@@ -0,0 +1,19 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        inter = []
+        for i in range(len(intervals)):
+            inter.append([intervals[i].start, intervals[i].end])
+        inter.sort()
+
+        for i in range(len(inter) - 1):
+            if inter[i][1] > inter[i + 1][0]:
+                return False
+        return True
